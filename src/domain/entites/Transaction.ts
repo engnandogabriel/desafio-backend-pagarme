f091ae04884a156884765fc4e0d0cc54c6ae5d7d
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-19 14:38:33 -0300

    test: added test to  entity Transacion

---
+++ src/domain/entites/Transaction.ts
@@ -0,0 +1,59 @@
+import crypto from 'crypto';
+
+export default class Transaction {
+    private id_transaction: string;
+    private id_client: string;
+    private description: string;
+    private value: number;
+    private method_payment: 'debit_card' | 'credit_card';
+    private card_number: string;
+    private name_owner: string;
+    private validate_date: string;
+    private cvv: string;
+
+    constructor(id_client: string, description: string, value: number, method_payment: 'debit_card' | 'credit_card', card_number: string, name_owner: string, validate_date: string, cvv: string) {
+        this.id_transaction = crypto.randomUUID();
+        this.id_client = id_client;
+        this.description = description;
+        this.value = value;
+        this.method_payment = method_payment;
+        this.name_owner = name_owner;
+        this.card_number = card_number;
+        this.validate_date = validate_date;
+        this.cvv = cvv;
+    }
+
+    getId(): string {
+        return this.id_transaction;
+    }
+
+    getClientId(): string {
+        return this.id_client;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+    getValue(): number {
+        return this.value;
+    }
+    getMethodPayment(): 'debit_card' | 'credit_card' {
+        return this.method_payment;
+    }
+
+    getCardNumber(): string {
+        return this.card_number;
+    }
+
+    getNameOwner(): string {
+        return this.name_owner;
+    }
+
+    getValidateDate(): string {
+        return this.validate_date;
+    }
+
+    getCvv(): string {
+        return this.cvv;
+    }
+}
