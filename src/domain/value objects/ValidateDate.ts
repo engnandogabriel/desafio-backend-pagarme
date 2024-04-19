f1d38f1ea5a71553266c13ed736b93c37be54704
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-19 14:53:38 -0300

    feat: added value object ValidateDate

---
+++ src/domain/value objects/ValidateDate.ts
@@ -0,0 +1,22 @@
+export default class ValidateDate {
+    private validate_date: string;
+    constructor(validate_date: string) {
+        this.validate_date = this.setValidateDate(validate_date);
+    }
+
+    private setValidateDate(date: string) {
+        const dateRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+        if (!dateRegex.test(date)) throw new Error('Date is invalid');
+        const [month, year] = date.split('/').map(Number);
+        const currentMonth = new Date().getMonth() + 1;
+        const currentYear = new Date().getFullYear() % 100;
+
+        if (year < currentYear || (year === currentYear && month < currentMonth) || month < 1 || month > 12) throw new Error('Date is invalid');
+
+        return date;
+    }
+
+    getValidateDate(): string {
+        return this.validate_date;
+    }
+}
