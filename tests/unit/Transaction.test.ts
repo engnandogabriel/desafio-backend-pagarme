0b930202d5c3558ec7f213df9e241bc36a58c2e2
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-19 14:46:44 -0300

    test: added test to card_number  in Transaction

--- tests/unit/Transaction.test.ts
+++ tests/unit/Transaction.test.ts
@@ -4,4 +4,5 @@ test('Deve criar uma transação', () => {
     const transaction = new Transaction('1234', 'Capa de celular', 30, 'debit_card', '1234 5678 7894 1234', 'John Doe', '01/26', '422');
     expect(transaction.getId()).toBeDefined();
     expect(transaction.getClientId()).toBe('1234');
+    expect(transaction.getCardNumber()).toBe('1234');
 });
