882f8c2fbe92063eae433ccf4d42a255d94d0fcb
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-19 14:56:10 -0300

    feat: added value object CVV

---
+++ src/domain/value objects/CVV.ts
@@ -0,0 +1,16 @@
+export default class CVV {
+    private cvv: string;
+
+    constructor(cvv: string) {
+        this.cvv = this.setCVV(cvv);
+    }
+
+    private setCVV(cvv: string) {
+        const regexCVV = /^\d{3,4}/;
+        if (!regexCVV.test(cvv)) throw new Error('CVV is invalid');
+        return cvv;
+    }
+    getCVV(): string {
+        return this.cvv;
+    }
+}
