a7e0b0ed24073e0b4a9c3501e4b79719bd1fd749
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-19 14:16:55 -0300

    feat: added test to create user with invalid email

--- tests/unit/User.test.ts
+++ tests/unit/User.test.ts
@@ -3,4 +3,11 @@ import User from '../../src/domain/entites/User';
 test('Deve criar um usuário', () => {
     const user = new User('john doe', 'john.doe@gmail.com');
     expect(user.getId()).toBeDefined();
+    expect(user.getEmail()).toBe('john.doe@gmail.com');
+});
+
+test('Não deve criar um usuário com email inválido', () => {
+    expect(() => new User('john doe', 'john.doegmail.com')).toThrow(
+        new Error('Invalid email')
+    );
 });
