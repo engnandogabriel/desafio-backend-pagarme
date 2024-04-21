e8f7e5f75d67dbc569f7f1cf0aa751431e0adecc
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-21 11:01:15 -0300

    feat: added entity Credit Payables

---
+++ src/domain/entites/Paybles/CreditPaybles.ts
@@ -0,0 +1,28 @@
+import crypto from 'crypto';
+import Payble from './Paybles';
+
+export default class CreditPaybles extends Payble {
+    private fee = 0.05;
+    private daysToAdd = 30;
+    private constructor(payble_id: string, transaction_id: string, client_id: string, value: number, status: string, type: string) {
+        super(payble_id, transaction_id, client_id, value, status, type);
+        this.setPaymentDate();
+        this.feeCalculator();
+    }
+
+    static create(transaction_id: string, client_id: string, value: number): Payble {
+        const id = crypto.randomUUID();
+        return new CreditPaybles(id, transaction_id, client_id, value, 'waiting_funds', 'credit_card');
+    }
+    static restory(payble_id: string, transaction_id: string, client_id: string, value: number, status: string, type: string): Payble {
+        return new CreditPaybles(payble_id, transaction_id, client_id, value, status, type);
+    }
+    protected setPaymentDate(): void {
+        const today = new Date();
+        const futureDate = new Date(today.getTime() + this.daysToAdd * 24 * 60 * 60 * 1000);
+        this.paymentDate = futureDate;
+    }
+    protected feeCalculator(): void {
+        this.value -= this.value * this.fee;
+    }
+}
