71d0fa311fb1073e7d238ccf5f361bf7970fc31e
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-21 13:26:50 -0300

    feat: added getByEmail UserRepository

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -3,5 +3,6 @@ import User from '../entites/User';
 export default interface UserRepository {
     save(data: User): Promise<void>;
     getById(user_id: string): Promise<User | void>;
+    getByEmail(email: string): Promise<User | void>;
     getAll(): Promise<Array<User>>;
 }
