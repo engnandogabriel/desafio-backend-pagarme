c271abaddedcd9185024ec74e66c6437a3131b41
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-22 16:05:34 -0300

    feat: added GetAmountPaid

---
+++ src/application/UseCase/GetAmountPaid.ts
@@ -0,0 +1,45 @@
+import HttpResponse from '../../domain/HttpServer/HttpResponse';
+import PayblesRepository from '../../domain/repository/PayblesRepository';
+import UserRepository from '../../domain/repository/UserRepository';
+import UseCase from './UseCase';
+
+export default class GetAmountPaid implements UseCase {
+    private userRepository: UserRepository;
+    private payblesRepository: PayblesRepository;
+    constructor(userRepository: UserRepository, payblesRepository: PayblesRepository) {
+        this.userRepository = userRepository;
+        this.payblesRepository = payblesRepository;
+    }
+    async execute(data: Input): Promise<HttpResponse> {
+        try {
+            const client = await this.userRepository.getById(data.client_id);
+            if (!client) return { statusCode: 404, body: 'Client not found' };
+            const paybles = await this.payblesRepository.getByMethodAndClient('paid', data.client_id);
+            let value = 0;
+            for (const payble of paybles) {
+                value += payble.getValue();
+            }
+            return {
+                statusCode: 200,
+                body: {
+                    status: 'paid',
+                    amount: value,
+                },
+            };
+        } catch (error) {
+            if (error instanceof Error) {
+                return {
+                    statusCode: 422,
+                    body: error.message,
+                };
+            }
+            return {
+                statusCode: 500,
+                body: 'Unexpected Error',
+            };
+        }
+    }
+}
+type Input = {
+    client_id: string;
+};
