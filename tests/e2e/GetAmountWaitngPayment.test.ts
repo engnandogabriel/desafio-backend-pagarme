28c4b4a2b289a24a61108709d409c5ef294fc01e
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-22 16:05:17 -0300

    test: added test to GetAmountWaitngPayment

---
+++ tests/e2e/GetAmountWaitngPayment.test.ts
@@ -0,0 +1,13 @@
+import GetAmountWaitngPayment from '../../src/application/UseCase/GetAmountWaitngPayment';
+import PayblesRepositoryMemory from '../../src/infra/Repository/PayblesRepositoryMemory';
+import UserRepositoryMemory from '../../src/infra/Repository/UserRepositoryMemory';
+
+test('Deve retorna o saldo já recebido', async () => {
+    const getAmountWaitngPayment = new GetAmountWaitngPayment(new UserRepositoryMemory(), new PayblesRepositoryMemory());
+    const Input = {
+        client_id: '642485af-9f85-4930-9ab9-f4c2d006d3c9',
+    };
+    const output = await getAmountWaitngPayment.execute(Input);
+    expect(output.statusCode).toBe(200);
+    expect(output.body.status).toBe('waiting_funds');
+});
