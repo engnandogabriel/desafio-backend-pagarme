302335a372174e859e4841eda8c37f00232244a6
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-22 14:56:04 -0300

    feat: added PayblesFactory implementation

---
+++ src/domain/entites/Paybles/PaybleFactory.ts
@@ -0,0 +1,13 @@
+import PaybleDTO from '../../DTO/PaybleDTO';
+import CreditPaybles from './CreditPaybles';
+import DebitPaybles from './DebitPaybles';
+import IPaybleFactory from './IPaybleFactory';
+import Payble from './Paybles';
+
+export default class PaybleFactory implements IPaybleFactory {
+    factory(type: string, payble: PaybleDTO): Payble {
+        if (type === 'credit_card') return CreditPaybles.restore(payble.payble_id, payble.transaction_id, payble.client_id, payble.value, payble.status, payble.type);
+        if (type === 'debit_card') return DebitPaybles.restore(payble.payble_id, payble.transaction_id, payble.client_id, payble.value, payble.status, payble.type);
+        throw new Error('Invalid payble type.');
+    }
+}
