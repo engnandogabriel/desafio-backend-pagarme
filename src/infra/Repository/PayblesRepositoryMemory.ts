0f32e3bfdcee3ffdf6dc176a7c804470320f83b7
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-22 16:05:49 -0300

    test: added test to GetAmountPaid

--- src/infra/Repository/PayblesRepositoryMemory.ts
+++ src/infra/Repository/PayblesRepositoryMemory.ts
@@ -6,8 +6,8 @@ export default class PayblesRepositoryMemory implements PayblesRepository {
     paybles: Array<Paybles>;
     constructor() {
         this.paybles = [
-            new PaybleFactory().factory('debit_card', { payble_id: '74c56ab-0f3c-4aa8-9fd5-098107bcb8cb', transaction_id: '92b80286-c56c-48c2-975d-e5ad817089fc', client_id: '157064e7-0ec2-40b6-b583-4ba017dfd53c', status: 'paid', type: 'debit_card', value: 24.75 }),
-            new PaybleFactory().factory('credit_card', { payble_id: '0e70ca3d-8324-42d6-8f08-2d64b6c39c67', transaction_id: 'e513f051-2ce7-4cc4-ae7e-a1bdd699bbbc', client_id: '642485af-9f85-4930-9ab9-f4c2d006d3c9', status: 'waiting_funds', type: 'credit_card', value: 95 }),
+            new PaybleFactory().factory('debit_card', { payble_id: '74c56ab-0f3c-4aa8-9fd5-098107bcb8cb', transaction_id: '92b80286-c56c-48c2-975d-e5ad817089fc', client_id: '157064e7-0ec2-40b6-b583-4ba017dfd53c', status: 'paid', type: 'debit_card', value: 24.25 }),
+            new PaybleFactory().factory('credit_card', { payble_id: '74c56ab-0f3c-4aa8-9fd5-098107bcb8cb', transaction_id: 'e513f051-2ce7-4cc4-ae7e-a1bdd699bbbc', client_id: '642485af-9f85-4930-9ab9-f4c2d006d3c9', status: 'waiting_funds', type: 'credit_card', value: 95 }),
         ];
     }
 
---
+++ tests/e2e/GetAmountPaid.test.ts
@@ -0,0 +1,13 @@
+import GetAmountPaid from '../../src/application/UseCase/GetAmountPaid';
+import PayblesRepositoryMemory from '../../src/infra/Repository/PayblesRepositoryMemory';
+import UserRepositoryMemory from '../../src/infra/Repository/UserRepositoryMemory';
+
+test('Deve retorna o saldo já recebido', async () => {
+    const getAmountPaid = new GetAmountPaid(new UserRepositoryMemory(), new PayblesRepositoryMemory());
+    const Input = {
+        client_id: '157064e7-0ec2-40b6-b583-4ba017dfd53c',
+    };
+    const output = await getAmountPaid.execute(Input);
+    expect(output.statusCode).toBe(200);
+    expect(output.body.status).toBe('paid');
+});
