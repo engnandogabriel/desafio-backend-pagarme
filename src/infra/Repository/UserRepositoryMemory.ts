b0317256c942cab186d553e82cf39c065771583a
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-22 11:47:19 -0300

    feat: added UserRepositoryMemory to test local

---
+++ src/infra/Repository/UserRepositoryMemory.ts
@@ -0,0 +1,25 @@
+import User from '../../domain/entites/User';
+import UserRepository from '../../domain/repository/UserRepository';
+export default class UserRepositoryMemory implements UserRepository {
+    users: Array<User>;
+    constructor() {
+        this.users = [
+            User.restore('157064e7-0ec2-40b6-b583-4ba017dfd53c', 'John Doe', 'john.doe@gmail.com'),
+            User.restore('642485af-9f85-4930-9ab9-f4c2d006d3c9', 'Fulano de Tal', 'fulano.de.tal@gmail.com'),
+        ];
+    }
+    async save(data: User): Promise<void> {
+        this.users.push(data);
+    }
+    async getById(user_id: string): Promise<void | User> {
+        const user = this.users.find((user) => user.getId() === user_id);
+        return user;
+    }
+    async getByEmail(email: string): Promise<void | User> {
+        const user = this.users.find((user) => user.getId() === email);
+        return user;
+    }
+    async getAll(): Promise<User[]> {
+        return this.users;
+    }
+}
