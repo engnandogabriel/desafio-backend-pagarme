0fd585d9a054943bf48f9b6321d9220b675f1ad3
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-23 14:24:02 -0300

    feat: added GetAllUser

---
+++ src/application/UseCase/GetAllUser.ts
@@ -0,0 +1,48 @@
+import HttpResponse from '../../domain/HttpServer/HttpResponse';
+import User from '../../domain/entites/User';
+import UserRepository from '../../domain/repository/UserRepository';
+import UseCase from './UseCase';
+
+export default class CreateUser implements UseCase {
+    private userRepository: UserRepository;
+    constructor(userRepository: UserRepository) {
+        this.userRepository = userRepository;
+    }
+    async execute(data: Input): Promise<HttpResponse> {
+        try {
+            const users = await this.userRepository.getAll();
+            const output: Array<Output> = [];
+            if (users) {
+                for (const user of users) {
+                    output.push({ id: user.getId(), name: user.getName(), email: user.getEmail() });
+                }
+            }
+            return {
+                statusCode: 200,
+                body: output,
+            };
+        } catch (error) {
+            if (error instanceof Error) {
+                return {
+                    statusCode: 422,
+                    body: error.message,
+                };
+            }
+            return {
+                statusCode: 500,
+                body: 'Unexpected Error',
+            };
+        }
+    }
+}
+
+type Input = {
+    name: string;
+    email: string;
+};
+
+type Output = {
+    id: string;
+    name: string;
+    email: string;
+};
