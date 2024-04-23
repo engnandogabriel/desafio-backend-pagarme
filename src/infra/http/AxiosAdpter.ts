045a717a9de7184c726d575b9f21ec52d13a0dfe
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-23 14:18:36 -0300

    feat: added AxiosAdapter implementation

---
+++ src/infra/http/AxiosAdpter.ts
@@ -0,0 +1,18 @@
+import HttpServer from './HttpServer';
+import express, { Request, Response } from 'express';
+export default class AxiosAdpter implements HttpServer {
+    app: any;
+    constructor() {
+        this.app = express();
+        this.app.use(express.json());
+    }
+    async on(method: string, url: string, callback: Function) {
+        this.app[method](url, async function (req: Request, res: Response) {
+            const output = await callback(req);
+            return res.status(output.statusCode).json(output.body);
+        });
+    }
+    listen(port: number): void {
+        this.app.listen(port);
+    }
+}
