5749b34b14129a227960ef255d1ed58d15baa29b
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-23 14:35:58 -0300

    feat: added TransactionController to manager routes

---
+++ src/infra/Controller/TransactionController.ts
@@ -0,0 +1,24 @@
+import CreateCreditTransaction from '../../application/UseCase/CreateCreditTransaction';
+import CreateDebitTransaction from '../../application/UseCase/CreateDebitTransaction';
+import HttpServer from '../http/HttpServer';
+
+export default class TransactionController {
+    private httpServer: HttpServer;
+    private createCreditTransaction: CreateCreditTransaction;
+    private createDebitTransaction: CreateDebitTransaction;
+    constructor(httpServer: HttpServer, createCreditTransaction: CreateCreditTransaction, createDebitTransaction: CreateDebitTransaction) {
+        this.httpServer = httpServer;
+        this.createCreditTransaction = createCreditTransaction;
+        this.createDebitTransaction = createDebitTransaction;
+
+        this.httpServer.on('post', '/transaction/credit_card', async (req: any) => {
+            const output = await this.createCreditTransaction.execute(req.body);
+            return output;
+        });
+
+        this.httpServer.on('post', '/transaction/credit_card', async (req: any) => {
+            const output = await this.createDebitTransaction.execute(req.body);
+            return output;
+        });
+    }
+}
