562cabb2f8bd57dc2d5a7efc7446f0555ecd9747
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-25 13:55:14 -0300

    feat added ServerError

---
+++ src/domain/Error/ServerError.ts
@@ -0,0 +1,7 @@
+export default class ServerError extends Error {
+    constructor(stack: string) {
+        super('Internal server error');
+        this.name = 'ServerError';
+        this.stack = stack;
+    }
+}
