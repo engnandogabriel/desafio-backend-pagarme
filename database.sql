3580110adbd3d8ab703127abce743004dce386d7
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-26 15:35:03 -0300

    Revert "refactor: change proprieties in database.sql"
    This reverts commit 5f0f3cb45dc02334e92e66dfaaf31c0e23db2530.

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS client (
 
 CREATE TABLE IF NOT EXISTS transactions (
     id_transactions VARCHAR(36) PRIMARY KEY,
-    id_client VARCHAR(36) REFERENCES client(id),
+    id_client VARCHAR(36) REFERENCES Client(id),
     description VARCHAR(255),
     value DECIMAL(10, 2),
     method_payment VARCHAR(50),
@@ -22,9 +22,8 @@ CREATE TABLE IF NOT EXISTS transactions (
 
 
 CREATE TABLE IF NOT EXISTS payables (
-    id_payble VARCHAR(36) PRIMARY KEY,
-    id_client VARCHAR(36) REFERENCES client(id),
-    id_transaction VARCHAR(36) REFERENCES transactions(id_transaction)
+    id VARCHAR(36) PRIMARY KEY,
+    id_client VARCHAR(36) REFERENCES Client(id),
     value DECIMAL(10, 2),
     status VARCHAR(36),
     payment_date DATE
