15006dfd14f4f7d5738f2046aac63a061e0b1af9
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-26 13:49:22 -0300

    feat added InvalidParamError in ValidateDate

--- src/domain/value objects/ValidateDate.ts
+++ src/domain/value objects/ValidateDate.ts
@@ -1,3 +1,5 @@
+import InvalidParamError from '../Error/InvalidParamError';
+
 export default class ValidateDate {
     private validate_date: string;
     constructor(validate_date: string) {
@@ -6,12 +8,12 @@ export default class ValidateDate {
 
     private setValidateDate(date: string) {
         const dateRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
-        if (!dateRegex.test(date)) throw new Error('Date is invalid');
+        if (!dateRegex.test(date)) throw new InvalidParamError('The date is invalid');
         const [month, year] = date.split('/').map(Number);
         const currentMonth = new Date().getMonth() + 1;
         const currentYear = new Date().getFullYear() % 100;
 
-        if (year < currentYear || (year === currentYear && month < currentMonth) || month < 1 || month > 12) throw new Error('Date is invalid');
+        if (year < currentYear || (year === currentYear && month < currentMonth) || month < 1 || month > 12) throw new Error('Period of date is invalid');
 
         return date;
     }
